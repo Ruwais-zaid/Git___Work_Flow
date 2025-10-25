e84bf0bfed5890c7143a72ac2b311f88e02357a5
Zaid Nasim <zaid.btech2022@sitpune.edu.in>
2025-10-25 20:29:34 +0530

    Add login usr,pass

--- login.js
+++ login.js
@@ -1,9 +1,9 @@
 console.log("Login in Module Working");
-const login = () => {
-    const obj = {username: "user1", password: "pass123"};
-    if(obj.username === "user1" && obj.password === "pass123"){
-        console.log("Login Successful");
-    } else {
-        console.log("Login Failed");
-    }
+
+
+
+const app = (user,pass) =>{
+    console.log("User:", user);
+
+    console.log("Pass:", pass);
 }
