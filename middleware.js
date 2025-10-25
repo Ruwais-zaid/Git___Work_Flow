76df8dba8595e99d93e9fbb3792ca89e56dac47c
Zaid Nasim <zaid.btech2022@sitpune.edu.in>
2025-10-25 15:18:23 +0530

    feature/middleware

---
+++ middleware.js
@@ -0,0 +1,13 @@
+const app = () => {
+
+    const autbearer = "Bearerabc123xyz456";
+    const token = (autbearer.split(" ")[0]).substring(6);
+    console.log(token);
+    if(!token){
+        console.log("No token found");
+    } else {
+        console.log("Token found:", token);
+
+}
+}
+app();
