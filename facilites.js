6ce3e802c94fae4dd65ae8e09ca8af382209cf87
Zaid Nasim <zaid.btech2022@sitpune.edu.in>
2025-10-26 14:26:47 +0530

    New facilites added

---
+++ facilites.js
@@ -0,0 +1,13 @@
+const facilities = {
+  wifi: true,
+  parking: false,
+  pool: true,
+  gym: false,
+};
+const { wifi, parking, pool, gym } = facilities;
+console.log(`Facilities:
+  Wifi: ${wifi}
+  Parking: ${parking}
+  Pool: ${pool}
+  Gym: ${gym}
+`);
