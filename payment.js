100dc0c611dff114775ba885c6a9a70a35b38002
Zaid Nasim <zaid.btech2022@sitpune.edu.in>
2025-10-27 14:18:57 +0530

    WIP All Payment itegration

--- payment.js
+++ payment.js
@@ -6,3 +6,21 @@ const payment = (paymenttype,user) =>{
 }
 
 payment("creditcard","john");
+
+const RazorpayPayment = (paymenttype,user) =>{
+    if(paymenttype === "Razorpay"){
+        return `payment done using ${paymenttype} by ${user}`;
+    }
+}
+
+RazorpayPayment("Razorpay","admin");
+
+const PaypalPayment = (paymenttype,user) =>{
+    if(paymenttype === "Paypal"){
+        return `payment done using ${paymenttype} by ${user}`;
+    }
+}
+
+PaypalPayment("Paypal","user");
+
+console.log("Payment Module Working");
